e76e6060c94d5dfd1e3331afca7736010c3437a4
kevinsshah <kevin.s.shah95@gmail.com>
2016-07-24 04:09:34 +0530

    some more changes

--- android/PlusButton2/app/src/main/java/com/example/android/plusbutton2/AppointmentActivity.java
+++ android/PlusButton2/app/src/main/java/com/example/android/plusbutton2/AppointmentActivity.java
@@ -1,52 +1,30 @@
 package com.example.android.plusbutton2;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
 import java.io.IOException;
-import java.util.Calendar;
 
 public class AppointmentActivity extends AppCompatActivity {
+    EditText loc,date1,time1;
 
-    private Calendar calendar;
-    private TextView dateView;
-    private int year, month, day;
-    private TextView time;
-    private String format = "";
-    private TimePicker timePicker1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
-        EditText location = (EditText) findViewById(R.id.setlocation);
-        String loc = location.getText().toString();
-        dateView = (TextView) findViewById(R.id.showdate);
-        calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        showDate(year, month + 1, day);
-        timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
-        time = (TextView) findViewById(R.id.showtime);
-        calendar = Calendar.getInstance();
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int min = calendar.get(Calendar.MINUTE);
-        showTime(hour, min);
+        loc = (EditText) findViewById(R.id.setlocation);
+        date1=(EditText)findViewById(R.id.setdate);
+        time1=(EditText)findViewById(R.id.settime);
     }
 
     public void submitApp(View v) {
-        final String date=dateView.getText().toString();
-        final String time1=time.getText().toString();
+        final String date=date1.getText().toString();
+        final String time=time1.getText().toString();
+        final String location = loc.getText().toString();
         new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... params) {
@@ -57,73 +35,15 @@ public class AppointmentActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 ;
+                data.addFormField("location",location);
                 data.addFormField("date",date);
-                data.addFormField("time",time1);
+                data.addFormField("time",time);
                 return null;
             }
         }.execute();
     }
 
 
-    @SuppressWarnings("deprecation")
-    public void setDate(View view) {
-        showDialog(999);
-
-    }
-
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        // TODO Auto-generated method stub
-        if (id == 999) {
-            return new DatePickerDialog(this, myDateListener, year, month, day);
-        }
-        return null;
-    }
-
-    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
-            // TODO Auto-generated method stub
-            // arg1 = year
-            // arg2 = month
-            // arg3 = day
-            showDate(arg1, arg2 + 1, arg3);
-        }
-    };
-
-    private void showDate(int year, int month, int day) {
-        dateView.setText(new StringBuilder().append(day).append("/")
-                .append(month).append("/").append(year));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-
-        return true;
-    }
-
-    public void setTime(View view) {
-        int hour = timePicker1.getCurrentHour();
-        int min = timePicker1.getCurrentMinute();
-        showTime(hour, min);
-    }
-
-    public void showTime(int hour, int min) {
-        if (hour == 0) {
-            hour += 12;
-            format = "AM";
-        } else if (hour == 12) {
-            format = "PM";
-        } else if (hour > 12) {
-            hour -= 12;
-            format = "PM";
-        } else {
-            format = "AM";
-        }
-        time.setText(new StringBuilder().append(hour).append(" : ").append(min)
-                .append(" ").append(format));
-    }
 
 
 }
