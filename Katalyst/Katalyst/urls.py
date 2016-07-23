3af1ecf10f0cadefb3bcfbbad9a30f4c8018a54d
Shyam <s4shyam95@gmail.com>
2016-07-24 02:55:29 +0530

    Django Enters

---
+++ Katalyst/Katalyst/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from KatalystApp.admin import admin_site
+urlpatterns = patterns('',
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^updateGcmMentor/$', 'KatalystApp.views.updateGcmMentor', name='Mentor update gcm'),
+    url(r'^updateGcmMentee/$', 'KatalystApp.views.updateGcmMentee', name='Mentee update gcm'),
+    url(r'^getMyChatsMentor/$', 'KatalystApp.views.getMyChatsMentor', name='getMyChats'),
+    url(r'^getMyChatsMentee/$', 'KatalystApp.views.getMyChatsMentee', name='getMyChats'),
+    url(r'^getChatHistoryMentor/$', 'KatalystApp.views.getChatHistoryMentor', name='getMyChatsHistory'),
+    url(r'^getChatHistoryMentee/$', 'KatalystApp.views.getChatHistoryMentee', name='getMyChatsHistory'),
+    url(r'^addQueryMessageMentor/$', 'KatalystApp.views.addQueryMessageMentor', name='addMsg'),
+    url(r'^addQueryMessageMentee/$', 'KatalystApp.views.addQueryMessageMentee', name='addMsg'),
+    url(r'^dashboard/$', 'KatalystApp.views.dashboard', name='dashboard'),
+    url(r'^mentor/$', 'KatalystApp.views.mentor', name='mentor'),
+    url(r'^mentee/$', 'KatalystApp.views.mentee', name='mentee'),
+    url(r'^getGoals/$', 'KatalystApp.views.getGoals', name='getGoals'),
+    url(r'^getSms/$', 'KatalystApp.views.getSMS', name='getSms'),
+    url(r'^allMentors/$', 'KatalystApp.views.allMentors', name='allMentors'),
+    url(r'^allMentees/$', 'KatalystApp.views.allMentees', name='allMentees'),
+    url(r'^meeting/$', 'KatalystApp.views.meeting', name='meeting'),
+    url(r'^analytics/$', 'KatalystApp.views.analytics', name='analytics'),
+    url(r'^analytic/$', 'KatalystApp.views.analytic', name='analytic'),
+
+
+
+)
---
+++ Katalyst/KatalystApp/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Qualification(models.Model):
+    institute = models.CharField(max_length=512,default=None, null=True, blank=True)
+    degree = models.CharField(max_length=512,default=None, null=True, blank=True)
+    start_date = models.IntegerField(max_length=5,default=None, null=True, blank=True)
+    end_date = models.IntegerField(max_length=5,default=None, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.institute + " " + self.degree
+
+
+class Mentor(models.Model):
+    first_name = models.CharField(max_length=512,default=None, null=True, blank=True)
+    last_name = models.CharField(max_length=512,default=None, null=True, blank=True)
+    email = models.CharField(max_length=512,default=None, null=True, blank=True)
+    mobile = models.CharField(max_length=10,default=None, null=True, blank=True)
+    GENDER_CHOICES = ((1, 'Male'), (2, 'Female'))
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+    age = models.IntegerField(max_length=4, blank=True, null=True, default=None)
+    location = models.CharField(max_length=512,default=None, null=True, blank=True)
+    max_num_of_students = models.IntegerField(max_length=1)
+    occupation = models.CharField(max_length=512,default=None, null=True, blank=True)
+    qualifications = models.ManyToManyField(Qualification)
+    gcm = models.CharField(max_length=512,default=None, null=True, blank=True)
+    uuid = models.CharField(max_length=512,default=None, null=True, blank=True)
+    django_user = models.OneToOneField(User)
+
+    def __unicode__(self):
+        return self.first_name+" "+self.last_name
+
+class Mentee(models.Model):
+    first_name = models.CharField(max_length=512,default=None, null=True, blank=True)
+    last_name = models.CharField(max_length=512,default=None, null=True, blank=True)
+    mobile = models.CharField(max_length=10,default=None, null=True, blank=True)
+    email = models.CharField(max_length=512,default=None, null=True, blank=True)
+    GENDER_CHOICES = ((1, 'Male'), (2, 'Female'))
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+    age = models.IntegerField(max_length=4, blank=True, null=True, default=None)
+    location = models.CharField(max_length=512,default=None, null=True, blank=True)
+    gcm = models.CharField(max_length=512,default=None, null=True, blank=True)
+    uuid = models.CharField(max_length=512,default=None, null=True, blank=True)
+    assigned_mentor = models.ForeignKey(Mentor)
+    django_user = models.OneToOneField(User)
+
+    def __unicode__(self):
+        return self.first_name+" "+self.last_name
+
+
+class Review(models.Model):
+    mentor = models.ForeignKey(Mentor)
+    mentee = models.ForeignKey(Mentee)
+    rating = models.IntegerField(max_length=3)
+    text = models.CharField(max_length=1024)
+
+    def __unicode__(self):
+        return self.mentor.first_name + " to " + self.mentee.first_name
+
+class Goals(models.Model):
+    name = models.CharField(max_length=512,default=None, null=True, blank=True)
+    description = models.CharField(max_length=1024,default=None, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Appointment(models.Model):
+    location = models.CharField(max_length=512,default=None, null=True, blank=True)
+    mentor = models.ForeignKey(Mentor)
+    mentee = models.ForeignKey(Mentee)
+    datetime = models.DateTimeField()
+    goals = models.ManyToManyField(Goals)
+
+    def __unicode__(self):
+        return self.mentor.first_name + " and " + self.mentee.first_name+"'s meeting"
+
+class Feedback(models.Model):
+    appointment = models.ForeignKey(Appointment)
+    rating = models.IntegerField(max_length=3)
+    text = models.CharField(max_length=1024)
+
+    def __unicode__(self):
+        return self.appointment.mentee.first_name + " to " + self.appointment.mentor.first_name
+
+class AppointmentChat(models.Model):
+    appointment = models.ForeignKey(Appointment)
+    SENDER_CHOICES = ((1, 'Mentor'), (2, 'Mentee'))
+    sender = models.IntegerField(choices=SENDER_CHOICES)
+    text = models.CharField(max_length=512,default=None, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.appointment.mentor.first_name + " and " + self.appointment.mentee.first_name + "'s chat"
+
+
+
+class Reminders(models.Model):
+    mentor = models.ForeignKey(Mentor)
+    mentee = models.ForeignKey(Mentee)
+    datetime = models.DateTimeField()
+    for_appointment = models.ForeignKey(Appointment)
+
+    def __unicode__(self):
+        return "Reminder for " + self.mentor.first_name + " and " + self.mentee.first_name + "'s appointment"
