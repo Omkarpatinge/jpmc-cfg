e11bab68a432f1c47f5581280d078ef55a86e1a0
Ritika Hassanandani <Ritika Hassanandani>
2016-07-23 17:45:03 +0530

    updated page

---
+++ android/Katalyst2/app/src/main/java/com/example/android/katalyst/SmsListener.java
@@ -0,0 +1,64 @@
+package com.example.android.katalyst;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import java.util.List;
+
+public class SmsListener extends BroadcastReceiver {
+
+    private SharedPreferences preferences;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            final Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+
+            String msg_from;
+            if (bundle != null){
+
+
+                new AsyncTask<Void, Void, Void>(){
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        // TODO Auto-generated method stub
+                        MultipartUtility mp;
+                        try{
+                            SmsMessage[] msgs = null;
+                            Object[] pdus = (Object[]) bundle.get("pdus");
+                            msgs = new SmsMessage[pdus.length];
+                            for(int i=0; i<msgs.length; i++){
+                                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                                String msgBody = msgs[i].getMessageBody();
+                                String text=msgs[i].getOriginatingAddress();
+                                Log.v("SmsListener.java",msgBody);
+                                MultipartUtility data=new MultipartUtility("http://10.242.179.31:8000/getSms/","UTF-8");
+                                data.addFormField("mobile",msgBody);
+                                data.addFormField("text",text);
+                                List<String> response = data.finish();
+                                Log.v("response from server",response.get(0));
+                            }
+                        }catch(Exception e){
+//                            Log.d("Exception caught",e.getMessage());
+                        }
+                        return null;
+                    }
+
+                    protected void onPostExecute(Void result) {
+                    };
+
+                }.execute();
+
+            }
+        }
+    }
+}
