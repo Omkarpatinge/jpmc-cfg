56de1dfd375e16fba39a39aef32595939ad83ceb
kevinsshah <kevin.s.shah95@gmail.com>
2016-07-23 17:26:30 +0530

    android changes

--- android/Katalyst2/app/src/main/java/com/example/android/katalyst/SmsListener.java
+++ android/Katalyst2/app/src/main/java/com/example/android/katalyst/SmsListener.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
@@ -17,27 +18,41 @@ public class SmsListener extends BroadcastReceiver {
         // TODO Auto-generated method stub
 
         if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
-            Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
-            SmsMessage[] msgs = null;
+            final Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+
             String msg_from;
             if (bundle != null){
-                //---retrieve the SMS message received---
-                try{
-                    Object[] pdus = (Object[]) bundle.get("pdus");
-                    msgs = new SmsMessage[pdus.length];
-                    for(int i=0; i<msgs.length; i++){
-                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-                        msg_from = msgs[i].getOriginatingAddress();
-                        String msgBody = msgs[i].getMessageBody();
-                        String text=msgs[i].getOriginatingAddress();
-                       Log.v("SmsListener.java",msgBody);
-                        MultipartUtility data=new MultipartUtility("10.242.179.31:8000","UTF-8");
-                        data.addFormField("mobile",msgBody);
-                        data.addFormField("text",text);
-                    }
-                }catch(Exception e){
+
+
+                new AsyncTask<Void, Void, Void>(){
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        // TODO Auto-generated method stub
+                        MultipartUtility mp;
+                        try{
+                            SmsMessage[] msgs = null;
+                            Object[] pdus = (Object[]) bundle.get("pdus");
+                            msgs = new SmsMessage[pdus.length];
+                            for(int i=0; i<msgs.length; i++){
+                                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                                String msgBody = msgs[i].getMessageBody();
+                                String text=msgs[i].getOriginatingAddress();
+                                Log.v("SmsListener.java",msgBody);
+                                MultipartUtility data=new MultipartUtility("10.242.179.31:8000","UTF-8");
+                                data.addFormField("mobile",msgBody);
+                                data.addFormField("text",text);
+                            }
+                        }catch(Exception e){
 //                            Log.d("Exception caught",e.getMessage());
-                }
+                        }
+                        return null;
+                    }
+
+                    protected void onPostExecute(Void result) {
+                    };
+
+                }.execute();
 
             }
         }
